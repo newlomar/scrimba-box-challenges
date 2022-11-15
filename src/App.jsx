cadf185eee7cdc3613b1ddba88934a5ff5094a3e
Newton Lomar <newtonlomar16@gmail.com>
2022-11-15 10:18:45 -0300

    adding current box challenge 3.2 text

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import boxes from "./boxes"
 import Box from "./Box"
 
 export default function App() {
+    /**
+     * Challenge: Create a toggle() function that logs
+     * "clicked!" to the console
+     * 
+     * Pass that function down to each of the Box components
+     * and set it up so when they get clicked it runs the function
+     */
+
     const [squares, setSquares] = React.useState(boxes)
     
     const squareElements = squares.map(square => (
